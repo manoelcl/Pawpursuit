03bc65e3d171cbf21068fcedc2dc0d3c2b225772
Manoel Castro <manoelcl@gmail.com>
2023-07-08 15:46:34 +0200

    ADD character rotation and movement trail particles

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 @export var speed = 200
+@onready var sprite = $Sprite2D
 
 func handleInput():
 	var moveDirection=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity=moveDirection*speed
-	
+	if (moveDirection.length())!=0:
+		sprite.rotation=moveDirection.angle()+PI/2
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
