8ea59fb442971209f9cca690e7bf929528f64ff6
Manoel Castro <manoelcl@gmail.com>
2023-07-08 18:29:05 +0200

    ADD cat enemy and enemy movement.

--- enemies/cat.gd
+++ enemies/cat.gd
@@ -1,6 +1,18 @@
 extends CharacterBody2D
-
+@export var speed = 1
+@onready var sprite = $Sprite2D
 @onready var animation = $AnimationPlayer
+@onready var player:Node =  get_node("../Player")
+
+func handleInput():
+	var moveDirection=player.global_position-position
+	velocity=moveDirection*speed
+	if (moveDirection.length())!=0:
+		sprite.rotation=moveDirection.angle()+PI/2
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
 
 func _ready():
 	animation.play("default")
--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 @export var speed = 200
 @onready var sprite = $Sprite2D
+@onready var animation =$AnimationPlayer
 
 func handleInput():
 	var moveDirection=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity=moveDirection*speed
 	if (moveDirection.length())!=0:
 		sprite.rotation=moveDirection.angle()+PI/2
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+
+func _ready():
+
+	animation.play("default")
+	pass
