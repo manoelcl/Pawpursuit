7fa84c1d6bd9d68f947310bc386defe64058dacb
Manoel Castro <manoelcl@gmail.com>
2023-07-08 21:12:14 +0200

    ADD normalized enemy movement

--- enemies/cat.gd
+++ enemies/cat.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
-@export var speed = 1
+@export var speed = 175
 @onready var sprite = $Sprite2D
 @onready var animation = $AnimationPlayer
 @onready var player:Node =  get_node("../Player")
 
 func handleInput():
-	var moveDirection=player.global_position-position
+	var moveDirection=(player.global_position-position).normalized()
 	velocity=moveDirection*speed
 	if (moveDirection.length())!=0:
 		sprite.rotation=moveDirection.angle()+PI/2
