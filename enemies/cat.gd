55679a302a3a2eff811f5f9fb6bbdc748fe51101
Manoel Castro <manoelcl@gmail.com>
2023-07-09 12:52:29 +0200

    ADD cat is also affected by traps

--- enemies/cat.gd
+++ enemies/cat.gd
@@ -1,20 +1,34 @@
 extends CharacterBody2D
-@export var speed = 175
+@export var normalSpeed = 175
+@export var decreasedSpeed = 125
 @onready var sprite = $Sprite2D
 @onready var animation = $AnimationPlayer
 @onready var player:Node =  get_node("../Player")
+var isSlow=0
 
 func handleInput():
-	var moveDirection=(player.global_position-position).normalized()
+	var moveDirection=(player.global_position-position).normalized()	
+	var speed=normalSpeed
+	if (isSlow>0):
+		isSlow-=1
+	if (get_meta("isDog")||isSlow>0):
+		speed=decreasedSpeed
 	if (player.get_meta("isDog")):
 		moveDirection=moveDirection*-1
 	velocity=moveDirection*speed
+	
 	if (moveDirection.length())!=0:
 		sprite.rotation=moveDirection.angle()+PI/2
 
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collider:Node2D = get_slide_collision(i).get_collider()
+		if (collider.get_meta("isTrap")==true):
+			isSlow=60
+			collider.queue_free()
+			print("Cat collided with Trap")
 
 func _ready():
 	animation.play("default")
